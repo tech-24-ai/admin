59d1d25d32cf89f6024b536285ecc0f2d71d9545
dhruvesh-intelloger <dhruvesh.gajjar@intelloger.com>
2024-05-17 19:16:10 +0530

    Changes in community post details page

--- src/pages/CommunityModule/CommunityPostReply/List.js
+++ src/pages/CommunityModule/CommunityPostReply/List.js
@@ -388,7 +388,7 @@ class CommunityPostReplyList extends React.PureComponent {
                                     <Grid item xs={3}>
                                         Posted by {form.visitor.name}
                                     </Grid>
-                                    <Grid item xs={2} style={{display: "flex"}}>
+                                    <Grid item xs={3} style={{display: "flex"}}>
                                         <AccessTime fontSize="small" style={{marginRight: 5}} />
                                         {moment(form.created_at).format("DD-MM-YYYY hh:m A")}
                                     </Grid>
